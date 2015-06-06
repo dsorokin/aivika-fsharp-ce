e72c5cd27376892ec71db4bb2d289af4bb54bf99
David Sorokin <david.sorokin@gmail.com>
2015-06-06 09:26:56 +0300

    Updated version number.

--- src/Simulation.Aivika/assembly.fs
+++ src/Simulation.Aivika/assembly.fs
@@ -27,11 +27,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Aivika for .NET")>]
 [<assembly: AssemblyDescriptionAttribute("A library for discrete event simulation, system dynamics and agent-based modeling")>]
 [<assembly: AssemblyInformationalVersionAttribute ("3.0")>]
-[<assembly: AssemblyVersionAttribute ("3.0.15.1")>]
-[<assembly: AssemblyFileVersionAttribute("3.0.15.1")>]
+[<assembly: AssemblyVersionAttribute ("3.0.15.2")>]
+[<assembly: AssemblyFileVersionAttribute("3.0.15.2")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright (C) 2009-2015 David Sorokin. All rights reserved.")>]
 
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.0.15.1"
+    let [<Literal>] Version = "3.0.15.2"
