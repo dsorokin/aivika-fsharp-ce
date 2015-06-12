3e944fb716f2d2fef61d655b1698035460630946
David Sorokin <david.sorokin@gmail.com>
2015-06-12 08:25:44 +0300

    Updated example.

---
+++ examples/MachineBreakdowns/ExperimentGtk.fs
@@ -0,0 +1,47 @@
+
+namespace Simulation.Aivika.Examples
+
+open System
+open System.Web.UI
+
+open Simulation.Aivika
+open Simulation.Aivika.Results
+open Simulation.Aivika.Experiments
+open Simulation.Aivika.Experiments.Web
+open Simulation.Aivika.Charting.Gtk.Web
+
+module Experiment =
+
+    [<EntryPoint>]
+    let main args =
+
+        let experiment = Experiment ()
+
+        experiment.Specs <- Model.specs
+        experiment.RunCount <- 1000
+
+        let inputQueue = ResultSet.findByName "inputQueue"
+
+        let machineSettingUp = ResultSet.findByName "machineSettingUp"
+        let machineProcessing = ResultSet.findByName "machineProcessing"
+
+        let jobsInterrupted = ResultSet.findByName "jobsInterrupted"
+        let jobsCompleted = ResultSet.findByName "jobsCompleted"
+
+        let providers =
+            [ExperimentProvider.experimentSpecs;
+             ExperimentProvider.infiniteQueue inputQueue;
+             ExperimentProvider.server machineSettingUp;
+             ExperimentProvider.server machineProcessing;
+             ExperimentProvider.description jobsInterrupted;
+             ExperimentProvider.lastValueStats jobsInterrupted;
+             ExperimentProvider.arrivalTimer jobsCompleted]
+
+        experiment.RenderHtml (Model.model, providers)
+            |> Async.RunSynchronously
+
+        Console.WriteLine()
+        Console.WriteLine("Press Enter...")
+        Console.ReadLine () |> ignore
+
+        0
