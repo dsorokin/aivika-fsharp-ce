e772d0bd107fc9126cdfd562ca0369175a030656
David Sorokin <david.sorokin@gmail.com>
2015-12-03 08:34:57 +0300

    Updated the example solution files.

--- examples/BouncingBall/ExperimentRun.fs
+++ examples/BouncingBall/ExperimentRun.fs
@@ -6,13 +6,32 @@ open System.Web.UI
 
 open Simulation.Aivika
 open Simulation.Aivika.Results
+open Simulation.Aivika.Experiments
+open Simulation.Aivika.Experiments.Web
+open Simulation.Aivika.Charting.Web
 
-module Terminal =
+module Experiment =
 
     [<EntryPoint>]
     let main args =
 
-        ResultSet.printInStopTime Model.specs Model.model
+        let experiment = Experiment ()
+
+        experiment.Specs <- Model.specs
+        experiment.RunCount <- 1
+
+        let x  = ResultSet.findByName "x"
+        let v  = ResultSet.findByName "v"
+        let xv = ResultTransform.append x v
+
+        let providers =
+            [ExperimentProvider.experimentSpecs;
+             ExperimentProvider.description xv;
+             ExperimentProvider.timeSeries x;
+             ExperimentProvider.timeSeries v]
+
+        experiment.RenderHtml (Model.model, providers)
+            |> Async.RunSynchronously
 
         Console.WriteLine()
         Console.WriteLine("Press Enter...")
