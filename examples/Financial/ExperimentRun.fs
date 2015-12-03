cb4d2ccdedbc8a9442b7a60080bc1500da1cddb5
David Sorokin <david.sorokin@gmail.com>
2015-12-03 19:57:18 +0300

    Updated the example solution files.

--- examples/Financial/ExperimentRun.fs
+++ examples/Financial/ExperimentRun.fs
@@ -6,13 +6,43 @@ open System.Web.UI
 
 open Simulation.Aivika
 open Simulation.Aivika.Results
+open Simulation.Aivika.Experiments
+open Simulation.Aivika.Experiments.Web
+open Simulation.Aivika.Charting.Web
 
-module Terminal =
+module Experiment =
 
     [<EntryPoint>]
     let main args =
 
-        ResultSet.printInStopTime Model.specs (Model.model Model.defaultParams)
+        let experiment = Experiment ()
+
+        experiment.Specs <- Model.specs
+        experiment.RunCount <- 1000
+
+        let income = 
+            [ResultSet.findByName "netIncome";
+             ResultSet.findByName "netCashFlow"]
+                |> ResultTransform.concat
+        
+        let cashFlow =
+            [ResultSet.findByName "npvIncome";
+             ResultSet.findByName "npvCashFlow"]
+                |> ResultTransform.concat
+
+        let providers =
+            [ExperimentProvider.experimentSpecs;
+             ExperimentProvider.description income;
+             ExperimentProvider.deviationChart income
+             ExperimentProvider.lastValueStats income;
+             ExperimentProvider.lastValueHistogram income;
+             ExperimentProvider.description cashFlow;
+             ExperimentProvider.deviationChart cashFlow;
+             ExperimentProvider.lastValueStats cashFlow;
+             ExperimentProvider.lastValueHistogram cashFlow]
+
+        experiment.RenderHtml (Model.model Model.randomParams, providers)
+            |> Async.RunSynchronously
 
         Console.WriteLine()
         Console.WriteLine("Press Enter...")
