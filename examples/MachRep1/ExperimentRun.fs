4c849fedb70168f53a353ceedb33054aee640a70
David Sorokin <david.sorokin@gmail.com>
2015-12-03 20:10:43 +0300

    Updated the example solution files.

--- examples/MachRep1/ExperimentRun.fs
+++ examples/MachRep1/ExperimentRun.fs
@@ -6,13 +6,33 @@ open System.Web.UI
 
 open Simulation.Aivika
 open Simulation.Aivika.Results
+open Simulation.Aivika.Experiments
+open Simulation.Aivika.Experiments.Web
+open Simulation.Aivika.Charting.Web
 
-module Terminal =
+module Experiment =
 
     [<EntryPoint>]
     let main args =
 
-        ResultSet.printInStopTime Model.specs Model.model
+        let experiment = Experiment ()
+
+        experiment.Specs <- Model.specs
+        experiment.RunCount <- 1000
+
+        let provider1 = ExperimentSpecsProvider ()
+        let provider2 = DeviationChartProvider ()
+        let provider3 = LastValueStatsProvider ()
+        let provider4 = LastValueHistogramProvider ()
+
+        let providers =
+            [ provider1 :> IExperimentProvider<HtmlTextWriter>;
+              provider2 :> IExperimentProvider<HtmlTextWriter>;
+              provider3 :> IExperimentProvider<HtmlTextWriter>;
+              provider4 :> IExperimentProvider<HtmlTextWriter> ]
+
+        experiment.RenderHtml (Model.model, providers)
+            |> Async.RunSynchronously
 
         Console.WriteLine()
         Console.WriteLine("Press Enter...")
