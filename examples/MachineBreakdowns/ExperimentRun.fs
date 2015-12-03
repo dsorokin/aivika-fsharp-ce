73f1864984f4dd2169e10e2289190737d2e496d8
David Sorokin <david.sorokin@gmail.com>
2015-12-03 20:35:04 +0300

    Updated the example solution files.

--- examples/MachineBreakdowns/ExperimentRun.fs
+++ examples/MachineBreakdowns/ExperimentRun.fs
@@ -6,13 +6,39 @@ open System.Web.UI
 
 open Simulation.Aivika
 open Simulation.Aivika.Results
+open Simulation.Aivika.Experiments
+open Simulation.Aivika.Experiments.Web
+open Simulation.Aivika.Charting.Web
 
-module Terminal =
+module Experiment =
 
     [<EntryPoint>]
     let main args =
 
-        ResultSet.printInStopTime Model.specs Model.model
+        let experiment = Experiment ()
+
+        experiment.Specs <- Model.specs
+        experiment.RunCount <- 1000
+
+        let inputQueue = ResultSet.findByName "inputQueue"
+
+        let machineSettingUp = ResultSet.findByName "machineSettingUp"
+        let machineProcessing = ResultSet.findByName "machineProcessing"
+
+        let jobsInterrupted = ResultSet.findByName "jobsInterrupted"
+        let jobsCompleted = ResultSet.findByName "jobsCompleted"
+
+        let providers =
+            [ExperimentProvider.experimentSpecs;
+             ExperimentProvider.infiniteQueue inputQueue;
+             ExperimentProvider.server machineSettingUp;
+             ExperimentProvider.server machineProcessing;
+             ExperimentProvider.description jobsInterrupted;
+             ExperimentProvider.lastValueStats jobsInterrupted;
+             ExperimentProvider.arrivalTimer jobsCompleted]
+
+        experiment.RenderHtml (Model.model, providers)
+            |> Async.RunSynchronously
 
         Console.WriteLine()
         Console.WriteLine("Press Enter...")
